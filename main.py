1c70ec87c2c93280e3ec2cbaf7cb0ef823479706
shuher237 <90762786+shuher237@users.noreply.github.com>
2022-11-12 17:16:00 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from transformers import pipeline
 translator = pipeline("translation_ru_to_en", model="Helsinki-NLP/opus-mt-ru-en")
 translation = translator("Это проект по программной инженерии студентов УрФУ Шухардина Александра и Эмиля Афлатунова")
-translation
+print(translation)
