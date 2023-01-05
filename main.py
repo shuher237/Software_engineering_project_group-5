5223af1508088c16678fb890f059a32a6be350cb
up9r4d3 <115419745+up9r4d3@users.noreply.github.com>
2023-01-05 17:51:41 +0300

    Removed the extra code

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #PZ_1
+
 '''from transformers import pipeline
 translator = pipeline("translation_ru_to_en", model="Helsinki-NLP/opus-mt-ru-en")
 translation = translator("Это проект по программной инженерии студентов УрФУ Шухардина Александра и Эмиля Афлатунова")
 print(translation)
 '''
+
+
+
 #PZ_2
+
 from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.efficientnet import preprocess_input
@@ -16,23 +21,6 @@ from PIL import Image
 
 # Загружаем предварительно обученную модель EfficientNetB0
 model = EfficientNetB0(weights='imagenet')
-
-# Прописываем путь к файлу с изображением
-img_path = 'plane.jpg'
-# Загружаем изображение в память
-# EfficientNetB0 рассчитана на изображения размером 224х224
-img = image.load_img(img_path, target_size=(224, 224))
-# Выполняем предварительную обработку изображения
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
-# Запускаем распознавание
-preds = model.predict(x)
-# Печатаем ТОП-3 класса с самой большой вероятностью
-classes = decode_predictions(preds, top=3)[0]
-for cl in classes:
-    print(cl[1], cl[2])
-    
     
 def load_image():
     """Создание формы для загрузки изображения"""
@@ -49,15 +37,12 @@ def load_image():
     else:
         return None
 
-# Выводим заголовок страницы средствами Streamlit     
-st.title('Классификация изображений')
-# Вызываем функцию создания формы загрузки изображения
-img = load_image()
 
 @st.cache(allow_output_mutation=True)
 def load_model():
     model = EfficientNetB0(weights='imagenet')
     return model
+
 
 def preprocess_image(img):
     img = img.resize((224, 224))
@@ -66,11 +51,13 @@ def preprocess_image(img):
     x = preprocess_input(x)
     return x
 
+
 def print_predictions(preds):
     classes = decode_predictions(preds, top=3)[0]
     for cl in classes:
         st.write(cl[1], cl[2])
-        
+
+
 # Загружаем предварительно обученную модель
 model = load_model()
 # Выводим заголовок страницы
